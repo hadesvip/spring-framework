b3128720fa984518ea99e501a66da726086d0059
wangyong <wyyundan@gmail.com>
2021-02-28 22:45:30 +0800

    #add:新增配置

--- spring-framework-samples/src/test/kotlin/org/springframework/samples/SpringBeanTest.kt
+++ spring-framework-samples/src/test/kotlin/org/springframework/samples/SpringBeanTest.kt
@@ -1,16 +1,22 @@
 package org.springframework.samples
 
 import org.junit.jupiter.api.Test
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.support.ClassPathXmlApplicationContext
 
 class SpringBeanTest {
 
 	@Test
-	fun beanTest(){
+	fun xmlApplicationContextTest(){
 		val applicationContext = ClassPathXmlApplicationContext("spring-bean.xml")
 		println("获取applicationContext对象")
 		print(applicationContext)
 
+	}
+
+	@Test
+	fun applicationContextTest(){
+		val applicationContext = AnnotationConfigApplicationContext();
 	}
 
 
